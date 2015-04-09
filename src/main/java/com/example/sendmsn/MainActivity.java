c167d70b4e9a283d87fa28d7ba1ec7cb32c62672
wengfada <wengfada@163.com>
2015-04-10 00:42:30 +0800

    2

--- src/main/java/com/example/sendmsn/MainActivity.java
+++ src/main/java/com/example/sendmsn/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends Activity {
 		et_content = (EditText) findViewById(R.id.et_content);
 		send = (Button) findViewById(R.id.send);
 		send = (Button) findViewById(R.id.send);
+		send = (Button) findViewById(R.id.send);
 		send.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
